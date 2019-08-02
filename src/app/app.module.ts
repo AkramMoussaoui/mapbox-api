0db25ff1ba16c7d61af7f777a401064b41c83304
Akram <akrammoussaoui14@gmail.com>
2019-08-02 21:53:05 +0100

    setting the map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgxMapboxGLModule } from "ngx-mapbox-gl";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken:
+        "pk.eyJ1IjoiYWtyYW0yNSIsImEiOiJjanl1M2ZyanYwOXc0M21wcml4dGRjbzcyIn0.Mqgp71vVsF24E9m6FFilCw",
+      geocoderAccessToken:
+        "pk.eyJ1IjoiYWtyYW0yNSIsImEiOiJjanl1M2ZyanYwOXc0M21wcml4dGRjbzcyIn0.Mqgp71vVsF24E9m6FFilCw"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
